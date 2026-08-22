05ff8842b85baf108135e6333d010f3fbad9e826
philipbiven-pixel <philipbiven@gmail.com>
2026-08-22 14:22:30 +0300

    Update redirect paths to components/home.html

--- frontend/app.js
+++ frontend/app.js
@@ -142,7 +142,7 @@ if (signupForm) {
 
     alert("Account created successfully, " + name + "!");
 
-    window.location.href = "home.html";
+    window.location.href = "components/home.html";
   });
 }
 
@@ -171,7 +171,7 @@ if (loginForm) {
      if (email === user.email && password === user.password) {
   alert("Login successful! Welcome, " + user.name + "!");
 
-  window.location.href = "home.html";
+  window.location.href = "components/home.html";
      }  else {
         alert("Incorrect email or password.");
       }
